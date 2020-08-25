75afde5fa3b1b51271b10957ace16116e5d5a667
Kurt Madel <kmadel@cloudbees.com>
2020-08-25 07:52:18 -0400

    Create ops-create-github-app-credential.groovy

---
+++ groovy/ops-create-github-app-credential.groovy
@@ -0,0 +1,25 @@
+import jenkins.*
+import jenkins.model.*
+import com.cloudbees.plugins.credentials.impl.*;
+import com.cloudbees.plugins.credentials.*;
+import com.cloudbees.plugins.credentials.domains.Domain;
+import org.jenkinsci.plugins.github_branch_source.GitHubAppCredentials;
+
+def gitHubOrg = "REPLACE_GITHUB_ORG"
+Jenkins jenkins = Jenkins.getInstance()
+
+def credentials = CredentialsProvider.lookupCredentials(
+        org.jenkinsci.plugins.github_branch_source.GitHubAppCredentials.class,
+        jenkins,
+        null,
+        null
+);
+
+def cred = credentials.findResult { it.id == "cloudbees-ci-workshop-github-app" ? it : null }
+
+Credentials newCred = new GitHubAppCredentials(CredentialsScope.GLOBAL, gitHubOrg, "${gitHubOrg} temp github app cred", cred.appID, cred. privateKey)
+newCred.setOwner(gitHubOrg)
+
+def domain = Domain.global()
+def store = jenkins.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
+store.addCredentials(domain, newCred)
