a75228ba8c16512b9a710b054a926fc6c02a9591
Kurt Madel <kmadel@cloudbees.com>
2021-01-26 15:01:07 -0500

    Create cbci-update-controller-security-opt-out-authz.groovy

---
+++ groovy/cbci-update-controller-security-opt-out-authz.groovy
@@ -0,0 +1,20 @@
+//only runs on CJOC
+import com.cloudbees.masterprovisioning.kubernetes.KubernetesMasterProvisioning
+import com.cloudbees.opscenter.server.casc.BundleStorage
+import com.cloudbees.opscenter.server.model.ManagedMaster
+import com.cloudbees.opscenter.server.model.OperationsCenter
+import com.cloudbees.opscenter.server.security.SecurityEnforcer
+import com.cloudbees.opscenter.server.sso.AuthorizationOptOutMode
+import jenkins.model.Jenkins
+import hudson.*
+import hudson.model.*
+
+String masterName = "REPLACE_CONTROLLER_NAME" 
+
+println "Master '${masterName}' already exists. Updating it."
+ManagedMaster managedMaster = OperationsCenter.getInstance().getConnectedMasters().find { it.name == masterName } as ManagedMaster
+
+//needed for CasC RBAC
+master.properties.replace(new SecurityEnforcer.OptOutProperty(AuthorizationOptOutMode.INSTANCE, false, null))
+master.save()
+master.onModified()
