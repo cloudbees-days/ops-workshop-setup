7f82f25342ef6bc0595e85270889ec776e599d18
Kurt Madel <kmadel@cloudbees.com>
2021-01-27 08:50:08 -0500

    Create reload-casc.groovy

---
+++ groovy/reload-casc.groovy
@@ -0,0 +1,14 @@
+import com.cloudbees.opscenter.client.casc.ConfigurationUpdaterHelper;
+import com.cloudbees.jenkins.cjp.installmanager.casc.ConfigurationBundle;
+import com.cloudbees.jenkins.cjp.installmanager.casc.ConfigurationBundleManager;
+import com.cloudbees.opscenter.client.casc.ConfigurationBundleService;
+import com.cloudbees.opscenter.client.casc.ConfigurationStatus;
+
+
+ConfigurationUpdaterHelper.checkForUpdates();
+ConfigurationBundleService service = ExtensionList.lookupSingleton(ConfigurationBundleService.class);
+ConfigurationBundle bundle = ConfigurationBundleManager.get().getConfigurationBundle();
+service.reloadIfIsHotReloadable(bundle);
+
+ConfigurationStatus.INSTANCE.setUpdateAvailable(false);
+ConfigurationStatus.INSTANCE.setOutdatedVersion(null);
