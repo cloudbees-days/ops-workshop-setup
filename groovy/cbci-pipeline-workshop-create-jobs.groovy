a70d7762028ba166e832d2fa7616d02d4519142b
Kurt Madel <kmadel@cloudbees.com>
2021-05-12 09:49:06 -0400

    Update cbci-pipeline-workshop-create-jobs.groovy

--- groovy/cbci-pipeline-workshop-create-jobs.groovy
+++ groovy/cbci-pipeline-workshop-create-jobs.groovy
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 Logger logger = Logger.getLogger("cbci-pipeline-workshop-create-jobs.groovy");
 
 def jenkins = Jenkins.instance;
-def templateFolderName = "template-jobs";
+def templateFolderName = "pipelines";
 def templateFolderJob = jenkins.getItemByFullName(templateFolderName);
 if (templateFolderJob == null) {
   //Pipeline Template Catalog
@@ -24,18 +24,23 @@ if (templateFolderJob == null) {
   catalog.updateFromSCM(); 
     
     def templateFolderXml = """
-    <com.cloudbees.hudson.plugins.folder.Folder plugin="cloudbees-folder@6.14">
-    <actions/>
-    <description></description>
-    <properties>
-      <com.cloudbees.hudson.plugins.folder.properties.SubItemFilterProperty plugin="cloudbees-folders-plus@3.10">
-        <allowedTypes>
-          <string>workshopCatalog/nodejs-app</string>
-        </allowedTypes>
-      </com.cloudbees.hudson.plugins.folder.properties.SubItemFilterProperty>
-    </properties>
-    <icon class="com.cloudbees.hudson.plugins.folder.icons.StockFolderIcon"/>
-  </com.cloudbees.hudson.plugins.folder.Folder>
+<com.cloudbees.hudson.plugins.folder.Folder plugin="cloudbees-folder@6.15">
+  <actions/>
+  <description></description>
+  <properties>
+    <com.cloudbees.hudson.plugins.folder.properties.SubItemFilterProperty plugin="cloudbees-folders-plus@3.12">
+      <allowedTypes>
+        <string>org.jenkinsci.plugins.workflow.job.WorkflowJob</string>
+        <string>workshopCatalog/maven</string>
+        <string>org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject</string>
+        <string>workshopCatalog/nodejs-app</string>
+        <string>com.cloudbees.hudson.plugins.folder.Folder</string>
+        <string>jenkins.branch.OrganizationFolder.org.jenkinsci.plugins.github_branch_source.GitHubSCMNavigator</string>
+      </allowedTypes>
+    </com.cloudbees.hudson.plugins.folder.properties.SubItemFilterProperty>
+  </properties>
+  <icon class="com.cloudbees.hudson.plugins.folder.icons.StockFolderIcon"/>
+</com.cloudbees.hudson.plugins.folder.Folder>
   """;
   
   def f = jenkins.createProjectFromXML(templateFolderName, new ByteArrayInputStream(templateFolderXml.getBytes("UTF-8")));
