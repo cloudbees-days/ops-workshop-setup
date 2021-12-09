1f7ea4347243eb5b7610424287be03379bad66fa
Kurt Madel <kmadel@cloudbees.com>
2021-12-09 05:36:52 -0500

    Create create-workshop-users.groovy

---
+++ groovy/create-workshop-users.groovy
@@ -0,0 +1,30 @@
+import jenkins.model.Jenkins
+
+import java.util.logging.Logger
+
+Logger logger = Logger.getLogger("create-workshop-users")
+
+String jenkinsUserId = "REPLACE_JENKINS_USER"
+def user = User.get(jenkinsUserId, false)
+try {
+  logger.info("user full name: " + user.getFullName())
+} catch(Exception ex) {
+  //
+}
+if(user==null) {
+  user = Jenkins.instance.securityRealm.createAccount(jenkinsUserId, "REPLACE_WORKSHOP_ATTENDEES_PASSWORD")
+}
+while(user == null) {
+  user = User.get(jenkinsUserId, false)
+}
+user.save()
+
+String adminUserId = "REPLACE_JENKINS_USER-admin"
+def adminUser = User.get(adminUserId, false)
+if(adminUser==null) {
+  Jenkins.instance.securityRealm.createAccount(adminUserId, "REPLACE_WORKSHOP_ATTENDEES_PASSWORD")
+}
+while(adminUser == null) {
+  adminUser = User.get(adminUserId, false)
+}
+adminUser.save()
